781adb338125c144b8352d8bebe4b710d3933a4c
AnoopVL <avlanjekar4@gmail.com>
2025-01-07 13:38:09 +0530

    content schema

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,5 @@
-import mongoose, { Schema, Document, Model } from "mongoose";
+import mongoose, { Schema, Document, Model, Types } from "mongoose";
+// import { enum } from "zod";
 import { enumUtil } from "zod/lib/helpers/enumUtil";
 // import { Schema, Document, Model } from "mongoose";
 // import { Schema, Document } from "mongoose";
@@ -12,23 +13,21 @@ interface IUser extends Document {
 
 interface Content extends Document {
   link: string;
-  // type: string | EnumType;
-  type: string;
+  type: string | ContentType;
   title: string;
-  tags: tagReference[];
-  userId: UserRef;
+  tags: Types.ObjectId[];
+  userId: Types.ObjectId;
 }
 
 interface Link extends Document {
   hash: string;
-  userId: UserRef;
+  userId: Types.ObjectId;
 }
 
 interface Tag extends Document {
   title: string;
 }
 
-// Define the User schema
 const userSchema: Schema<IUser> = new Schema<IUser>({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
@@ -40,9 +39,37 @@ const tagSchema: Schema<Tag> = new Schema<Tag>({
   title: { type: String, required: true, unique: true },
 });
 
+type ContentType = "image" | "video" | "article" | "audio";
+
+// const contentSchema = new Schema<Content>({
+//   link: { type: String, required: true },
+//   type: {
+//     type: String,
+//     enum: ["image", "video", "article", "audio"],
+//     required: true,
+//   },
+//   title: { type: String, required: true },
+//   tags: [{ type: Types.ObjectId, ref: "Tag" }],
+//   userId: { type: Types.ObjectId, ref: "User", required: true },
+// });
+
+const conetentSchema: Schema<Content> = new Schema<Content>({
+  link: { type: String, required: true },
+  type: {
+    type: String,
+    enum: ["image", "video", "article", "audio"],
+    required: true,
+  },
+  title: { type: String, required: true },
+  tags: [{ type: Types.ObjectId, ref: "Tag" }],
+  userId: { type: Types.ObjectId, ref: "User", required: true },
+});
+
 export const UserModel: Model<IUser> = mongoose.model<IUser>(
   "User",
   userSchema
 );
 
 export const TagModel: Model<Tag> = mongoose.model<Tag>("Tag", tagSchema);
+
+export const ContentModel: Model<Content> = mongoose.model<Content>("Content", conetentSchema);
